6c8a11ac4b5675df8109467b0c9e47df8ede4f40
MechaDragonX <sonicraghav@gmail.com>
2019-11-08 13:58:11 -0800

    Add project files. Contains Guitar files from textbook.

---
+++ src/com/mechadragonx/audio/Guitar37.java
@@ -0,0 +1,21 @@
+// skeleton version of the class
+
+package com.mechadragonx.audio;
+
+public class Guitar37 implements Guitar {
+    public static final String KEYBOARD =
+        "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";  // keyboard layout
+
+    @Override
+    public void playNote(int pitch) { }
+    @Override
+    public boolean hasString(char key) { return false; }
+    @Override
+    public void pluck(char key) { }
+    @Override
+    public double sample() { return 0; }
+    @Override
+    public void tic() { }
+    @Override
+    public int time() { return 0; }
+}
---
+++ src/com/mechadragonx/audio/Main.java
@@ -0,0 +1,8 @@
+package com.mechadragonx.audio;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+    }
+}
